a22b29cbc662ec491ac85b957461c27de2567e52
iZotope235 <mmnik84@mail.ru>
2023-06-22 23:06:12 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(("Привет Мир"));
+        short age = 18;
+        char sex = 'M';
+        short countOfBricks = 1556;
+        float countOfBacteria = 1e+7f;
+        long distanceToSun = 235246346;
+        boolean workPermit = true;
+        long fileSize = 3456343453635L;
     }
 }
