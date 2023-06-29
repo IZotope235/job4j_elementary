c37a9cc49195f219ab2d528cf1ca42a2c3e78096
iZotope235 <mmnik84@mail.ru>
2023-06-29 18:44:12 +0300

    6.0.3. Заполнение массива. [#226300  [#226300]]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Marat";
+        names[1] = "Andrey";
+        names[2] = "Alexey";
+        names[3] = "Alexandra";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
