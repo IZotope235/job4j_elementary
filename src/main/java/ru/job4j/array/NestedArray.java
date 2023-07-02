6823838c60ca85096bec8394ffb1fd23afc3a22c
iZotope235 <mmnik84@mail.ru>
2023-07-02 09:06:18 +0300

    6.6.1. Размер элемента двухмерного массива. [#333583  [#333583 #426928]]

--- src/main/java/ru/job4j/array/NestedArray.java
+++ src/main/java/ru/job4j/array/NestedArray.java
@@ -2,12 +2,9 @@ package ru.job4j.array;
 
 public class NestedArray {
     public static void main(String[] args) {
-    int[][] array = new int[][] {{1}, {2, 2}, {3, 3, 3}, {4, 4, 4, 4}};
-        for (int[] innerArray: array) {
+        int[][] array = new int[][]{{1}, {2, 2}, {3, 3, 3}, {4, 4, 4, 4}};
+        for (int[] innerArray : array) {
             System.out.println(innerArray.length);
-
         }
-
     }
-
 }
